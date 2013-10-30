fcc6d5ee702a116b059e0effe6a8e4ec6aa10e8b
Anthony Pipkin <a.pipkin@yahoo.com>
2013-10-30 17:12:13 -0400

    Fix typo causing errors when `Object.defineProperty` is commented out

--- Intl.js
+++ Intl.js
@@ -23,7 +23,7 @@ var
     // Naive defineProperty for compatibility
     defineProperty = Object.defineProperty || function (obj, name, desc) {
         if (desc.get && obj.__defineGetter__)
-            obj.__defineGetter(name, desc.get);
+            obj.__defineGetter__(name, desc.get);
         else if (desc.value || desc.get)
             obj[name] = desc.value || desc.get;
     },
