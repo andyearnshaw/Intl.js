deae3fe4fbbb48356eeeabe07fce4c0ed7f92a00
Drew Folta <drew@folta.net>
2014-01-09 08:53:10 -0800

    use new tests/test262/pages to run tests

---
+++ tests/noderunner.js
@@ -0,0 +1,73 @@
+
+
+var LIBS = {
+        fs:     require('fs'),
+        path:   require('path'),
+        vm:     require('vm')
+    },
+    TEST_DIR = __dirname + '/test262/pages';
+
+
+// returns Error if test threw one
+function runTest(testPath) {
+    var content;
+    var sandbox = {};
+    content = LIBS.fs.readFileSync(LIBS.path.resolve(TEST_DIR, testPath)).toString();
+    try {
+        LIBS.vm.runInNewContext(content, sandbox, testPath);
+    } catch (err) {
+        return err;
+    }
+    return;
+}
+
+
+function listTests() {
+    var tests = [],
+        todo = [ '.' ],
+        doing,
+        path;
+    while (todo.length) {
+        doing = todo.shift();
+        path = LIBS.path.resolve(TEST_DIR, doing);
+        stat = LIBS.fs.statSync(path);
+        if (stat.isFile()) {
+            tests.push(doing);
+            continue;
+        }
+        if (stat.isDirectory()) {
+            todo = todo.concat(LIBS.fs.readdirSync(path).map(function(a) {
+                return LIBS.path.join(doing, a);
+            }));
+        }
+    }
+    return tests;
+}
+
+
+function main() {
+    var tests,
+        passCount = 0,
+        failCount = 0;
+    tests = listTests();
+    tests.sort();
+    tests.forEach(function(testPath) {
+        var name,
+            err;
+        name = LIBS.path.basename(testPath, LIBS.path.extname(testPath));
+        err = runTest(testPath);
+        if (err) {
+            console.log(name, '-- FAILED', err.message);
+            failCount++;
+        } else {
+            console.log(name);
+            passCount++;
+        }
+    });
+    console.log('total ' + (tests.length) + ' -- passed ' + passCount + ' -- failed ' + failCount);
+    process.exit(failCount ? 1 : 0);
+}
+main();
+
+
+
