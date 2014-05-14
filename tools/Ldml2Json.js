878c8014fe8bd6907eb1927ee9b749ca9fdb7fda
Andy Earnshaw <andyearnshaw@gmail.com>
2014-05-14 22:28:41 +0100

    Normalises array and property order in data files

--- tools/Ldml2Json.js
+++ tools/Ldml2Json.js
@@ -185,7 +185,7 @@ function processObj(data) {
         // Get own property values, useful for converting object map to array when we
         // don't care about the keys.  Relies on predictable property ordering in V8.
         gopv = function (o) {
-            return o ? Object.getOwnPropertyNames(o).map(function (e) { return o[e]; }) : undefined;
+            return o ? Object.getOwnPropertyNames(o).map(function (e) { return o[e]; }).sort() : undefined;
         },
 
         // Copy numbering systems
@@ -294,10 +294,10 @@ function processObj(data) {
         ret.number.patterns.secondaryGroupSize = groups[1].length;
 
     // Copy the currency symbols
-    for (var k in data.numbers.currencies) {
+   gopn(data.numbers.currencies).forEach(function (k) {
         if (k !== data.numbers.currencies[k].symbol)
             ret.number.currencies[k] = data.numbers.currencies[k].symbol;
-    }
+    });
 
 
     // Copy the formatting information
