ad99ae9d13566cfdf9d36372c1c7f396e70ff114
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-04-27 14:17:26 +0200

    fix bug to check Intl object

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import IntlPolyfill from "./core.js";
 
 // hack to export the polyfill as global Intl if needed
-if (typeof Intl !== undefined) {
+if (typeof Intl !== 'undefined') {
     Intl = IntlPolyfill;
     IntlPolyfill.__applyLocaleSensitivePrototypes();
 }
