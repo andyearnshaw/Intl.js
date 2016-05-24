54ef5de115db96482a20b3b218c8ced86c6c81a3
Caridy <cpatino@salesforce.com>
2016-05-24 07:52:56 -0700

    forcing window.Intl to be defined if global Intl is undefined.

--- src/browser-main.js
+++ src/browser-main.js
@@ -3,7 +3,7 @@ import IntlPolyfill from "./core.js";
 // hack to export the polyfill as global Intl if needed
 if (typeof Intl === 'undefined') {
     try {
-        Intl = IntlPolyfill;
+        window.Intl = IntlPolyfill;
         IntlPolyfill.__applyLocaleSensitivePrototypes();
     } catch (e) {
         // can be read only property
