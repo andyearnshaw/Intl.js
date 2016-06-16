79c465796f48fac4ea483b151e04a13a881898ac
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-06-16 19:56:49 +0200

    tests: undefined variable "self" should be void 0 in the vm

--- tests/polyfilling.js
+++ tests/polyfilling.js
@@ -12,6 +12,7 @@ function assert(value, expected, message) {
 
 const context = new vm.createContext();
 var script = new vm.Script('this');
+context.self = void 0;
 const window = script.runInContext(context);
 
 const fs = require('fs');
@@ -22,10 +23,11 @@ window.window = window;  // circular, in case the polyfill uses window
 var originalIntl = window.Intl;
 var script = new vm.Script(code);
 script.runInContext(context);
-assert(typeof global.Intl, 'object', 'for this test to function, global.Intl is required');
+assert(typeof window.Intl, 'object', 'for this test to function, window.Intl is required');
 assert(typeof window.IntlPolyfill, 'object', 'polyfill should always add the custom global IntlPolyfill');
-assert(window.Intl, originalIntl, 'validating that the polyfilling process does not touch the original Intl value');
-
+if (originalIntl) {
+  assert(window.Intl, originalIntl, 'validating that the polyfilling process does not touch the original Intl value if it exists');
+}
 // second evaluation
 window.window = window;  // circular, in case the polyfill uses window
 window.Intl = undefined; // disabling Intl
